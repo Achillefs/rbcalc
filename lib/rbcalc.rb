92696a5a88fd9b8face200b800162275148fdc9f
Achilles Charmpilas <ac@humbuckercode.co.uk>
2014-05-13 10:28:41 +0200

    think I figured out compilation

--- lib/rbcalc.rb
+++ lib/rbcalc.rb
@@ -27,24 +27,12 @@ module Rbcalc
   
   def self.init!
     vpath = self.home.join('..','vendor','bcalc')
-    incpath = Pathname('/usr/local/include')
-    libpath = Pathname('/usr/local/lib')
     # prepare library for linking
     case Rbcalc.os
     when :macosx
-      unless File.exists?(libpath.join('libbcalcdds.dylib'))
-        FileUtils.ln_s(vpath.join('libbcalcdds.dylib'),libpath.join('libbcalcdds.dylib')) 
-      end
-      unless File.exists?(incpath.join('bcalcdds.h'))
-        FileUtils.ln_s(vpath.join('bcalcdds.h'),incpath.join('bcalcdds.h')) 
-      end
+      # ok
     when :linux
-      unless File.exists?(libpath.join('libbcalcdds.so'))
-        FileUtils.ln_s(vpath.join('libbcalcdds.so'),libpath.join('libbcalcdds.so')) 
-      end
-      unless File.exists?(incpath.join('bcalcdds.h'))
-        FileUtils.ln_s(vpath.join('bcalcdds.h'),incpath.join('bcalcdds.h')) 
-      end
+      # ok
     else
       raise Error::WebDriverError, "Rbcalc cannot currently run on #{Rbcalc.os}"
     end
